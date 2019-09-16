19203f2a7d54d45d87674ee8d6c6e8d4f08ea666
Jonathan Ho <johnnyho82@gmail.com>
2019-09-16 19:21:14 -0400

    Set up redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 
+import { reducer } from './reducers/';
+
 import Dashboard from './dashboard/Dashboard';
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Dashboard />
+  </Provider>,
+  document.getElementById('root'),
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const initialState = { locked: false, closed: false };
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
